b38ae75f1abc965a954dc1afc75ba7624d6c4650
high-g <altema1217bbfumi@live.jp>
2023-04-09 10:28:17 +0900

    modify: delete

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ const typeDefs = gql`
   type Mutation {
     createUser(name: String!, email: String!): User
     updateUser(id: Int!, name: String!): User
+    deleteUser(id: Int!): User
   }
 `
 
@@ -91,6 +92,13 @@ const resolvers = {
         },
         data: {
           name: args.name,
+        },
+      })
+    },
+    deleteUser: (_, args) => {
+      return prisma.user.delete({
+        where: {
+          id: args.id,
         },
       })
     },
