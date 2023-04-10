ed00ef130b844d1798bcd4c6efaa4c86074a9128
high-g <altema1217bbfumi@live.jp>
2023-04-10 11:47:29 +0900

    modify: ApolloClientを試した ok

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,25 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 })
 
-client
-  .query({
-    query: gql`
-      query GetUsers {
-        users {
-          id
-          name
-          email
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result))
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
