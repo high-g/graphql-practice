753167dd5ada268a85f73a96b8f3576159a27eec
high-g <altema1217bbfumi@live.jp>
2023-04-10 11:28:24 +0900

    modify: pnpmでパッケージを置き換え

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+client
+  .query({
+    query: gql`
+      query GetUsers {
+        users {
+          id
+          name
+          email
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result))
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
